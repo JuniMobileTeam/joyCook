3e5aae27e91e7281a71ce1f13f64a9930d264349
Thai-Construct <thai@construct.sg>
2017-11-09 20:14:43 +0700

    Index page - apply features list

---
+++ resources/views/elements/_page-header.blade.php
@@ -0,0 +1,4 @@
+<header class="page-head header-panel-absolute">
+  <!-- Logo & address-->
+  @include('elements/_logo-and-address')
+</header>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,11 @@
 @section('title', '| Home')
 
 @section('content')
-  @include('elements/_masthead-swiper')
-  @include('elements/_logo-and-address')
-  @include('elements/_greetings-statements')
+  @include('elements/_page-header')
+  <main class="page-content">
+    @include('elements/_masthead-swiper')
+    @include('elements/_greetings-statements')
+    @include('elements/_features-foods')
+  </main>
+
 @endsection
