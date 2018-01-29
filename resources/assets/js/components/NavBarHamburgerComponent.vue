f75668884dc4403acc1d77769d747a305fab2bb4
Thai Nguyen <thai@construct.sg>
2018-01-30 04:42:15 +0700

    Vue - add NavBarHamburgerComponent (working)

---
+++ resources/assets/js/components/NavBarHamburgerComponent.vue
@@ -0,0 +1,73 @@
+<template lang="html">
+  <div class="rd-navbar-wrap">
+    <nav data-lg-auto-height="true" data-md-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed"
+      data-lg-layout="rd-navbar-fixed" data-lg-device-layout="rd-navbar-fixed" data-lg-stick-up="false"
+      data-lg-hover-on="false" data-auto-height="false" class="rd-navbar rd-navbar-hamburger-menu rd-navbar-fixed">
+      <div class="rd-navbar-inner">
+        <div class="rd-navbar-panel">
+          <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar, .rd-navbar-nav-wrap">
+            <span></span>
+          </button><!-- end button.rd-navbar-toggle -->
+          <h4 class="panel-title veil-md">Home</h4>
+          <button data-rd-navbar-toggle=".rd-navbar-top-panel" class="rd-navbar-top-panel-toggle veil-md">
+            <span></span>
+          </button><!-- end button.veil-md -->
+          <div class="rd-navbar-top-panel">
+            <div class="rd-navbar-top-panel-left-part">
+              <ul class="list-unstyled">
+                <li>
+                  <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                    <div class="unit-left">
+                      <span class="icon mdi mdi-phone text-middle"></span>
+                    </div>
+                    <div class="unit-body">
+                      <a href="callto:#">+84 909 865 842</a>
+                      <a href="callto:#">+84 909 765 697</a>
+                    </div>
+                  </div><!-- end div.unit-spacing-xs -->
+                </li>
+
+                <li>
+                  <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                    <div class="unit-left">
+                      <span class="icon mdi mdi-map-marker text-middle"></span>
+                    </div>
+                    <div class="unit-body">
+                      <a href="#">The Melody Residences, đường Âu Cơ, quận Tân Phú, TP Hồ Chí Minh</a>
+                    </div>
+                  </div><!-- end div.unit-spacing-xs -->
+                </li>
+
+                <li>
+                  <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                    <div class="unit-left">
+                      <span class="icon mdi mdi-email-open text-middle"></span>
+                    </div>
+                    <div class="unit-body">
+                      <a href="mailto:#">hotro@joyofcook.vn</a>
+                    </div>
+                  </div>
+                </li>
+
+                <!-- Login/Register section -->
+                <div class="rd-navbar-top-panel-right-part">
+                  <div class="rd-navbar-top-panel-left-part">
+                    
+                  </div><!-- end div.rd-navbar-top-panel-left-part -->
+                </div><!-- end div.rd-navbar-top-panel-right-part -->
+              </ul><!-- end ul.list-unstyled -->
+            </div><!-- end rd-navbar-top-panel-left-part -->
+          </div><!-- end div.rd-navbar-top-panel -->
+        </div> <!-- end div.rd-navbar-panel -->
+      </div><!-- end div.rd-navbar-inner -->
+    </nav>
+  </div><!-- end div.rd-navbar-wrap -->
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
