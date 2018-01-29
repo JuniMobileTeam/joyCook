3d386cc044ad39f7edf5365a89c2383e844927d4
Thai Nguyen <thai@construct.sg>
2018-01-29 10:25:24 +0700

    Make GalleryRecipes works

--- resources/assets/js/components/GalleryRecipesComponent.vue
+++ resources/assets/js/components/GalleryRecipesComponent.vue
@@ -2,7 +2,19 @@
   <section>
     <div class="owl-carousel flickr" data-items="1" data-xs-items="2" data-sm-items="3" data-lg-items='4' data-xl-items="6" data-nav="false" data-dots="false" data-mouse-drag="true" data-photo-swipe-gallery='' data-flickr-tags="tm-61184" data-stage-padding="0" data-xl-stage-padding="0">
       <a class="thumbnail-default thumbnail-flickr" data-photo-swipe-item="" href="" data-image_c='href' data-size="320x320" data-type='flickr-item'>
-        <img width="320" height="320" data-title='alt', src='images/_blank.png', alt='', "data-image_z"= "src">
+        <img width="320" height="320" data-title='alt' src='images/_blank.png' alt='' data-image_z='src'>
+        <span class="icon fa-search-plus"></span>
+      </a>
+      <a class="thumbnail-default thumbnail-flickr" data-photo-swipe-item="" href="" data-image_c='href' data-size="320x320" data-type='flickr-item'>
+        <img width="320" height="320" data-title='alt' src='images/_blank.png' alt='' data-image_z='src'>
+        <span class="icon fa-search-plus"></span>
+      </a>
+      <a class="thumbnail-default thumbnail-flickr" data-photo-swipe-item="" href="" data-image_c='href' data-size="320x320" data-type='flickr-item'>
+        <img width="320" height="320" data-title='alt' src='images/_blank.png' alt='' data-image_z='src'>
+        <span class="icon fa-search-plus"></span>
+      </a>
+      <a class="thumbnail-default thumbnail-flickr" data-photo-swipe-item="" href="" data-image_c='href' data-size="320x320" data-type='flickr-item'>
+        <img width="320" height="320" data-title='alt' src='images/_blank.png' alt='' data-image_z='src'>
         <span class="icon fa-search-plus"></span>
       </a>
     </div>
